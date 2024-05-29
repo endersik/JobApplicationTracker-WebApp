a7c90b9435c8bb3420784dc96b691d6bf55e277b
Ender Şık <mailtoender@gmail.com>
2024-05-29 19:59:51 +0300

    Launched project with Application Entity

---
+++ Models/Entities/Application.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobApplicationTracker.Models.Entities
+{
+    public class Application
+    {
+        public Guid Id { get; set; }
+        public required string Company { get; set; }
+        public string? Position { get; set; }
+        public required DateOnly Deadline { get; set; }
+        public bool? Applied { get; set; }
+    }
+}
